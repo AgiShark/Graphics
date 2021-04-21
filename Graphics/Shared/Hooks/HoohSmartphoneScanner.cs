498c8a74f20486e2a8c2c728aa1d984a9f3cdbce
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-04-21 16:42:51 -0600

    Fix so it actually scans the workspace for phones instead of just the root...

--- Graphics/Shared/Hooks/HoohSmartphoneScanner.cs
+++ Graphics/Shared/Hooks/HoohSmartphoneScanner.cs
@@ -21,26 +21,38 @@ namespace Graphics
                 GameObject commonSpace = GameObject.Find("CommonSpace");
                 if (commonSpace != null)
                 {
-                    foreach (Transform child in commonSpace.transform)
-                    {
-                        if (child.gameObject.name == "camera_colorable")
-                        {
-                            Camera cam = child.gameObject.GetComponentInChildren<Camera>();
-                            if (cam != null && cam.GetComponent<SSS>() == null)
-                            {
-                                SSS camSSS = cam.gameObject.AddComponent<SSS>();
-                                camSSS.enabled = true;
-                                camSSS.Enabled = true;
-                                SSSManager.RegisterAdditionalInstance(camSSS);
-                                Graphics.Instance.Log.LogInfo($"Adding SSS Component to Camera: {cam.name} GO: {cam.gameObject.name}");
-                            }
-                        }
-                    }
+                    RecurseScan(commonSpace.transform);
                 }
 
                 yield return new WaitForSecondsRealtime(5);
             }
         }
+
+        private void RecurseScan(Transform transform)
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.name == "camera_colorable")
+                {
+                    AddSSSCam(child);
+                }
+                RecurseScan(child);
+            }
+        }
+
+        private void AddSSSCam(Transform child)
+        {
+            Camera cam = child.gameObject.GetComponentInChildren<Camera>();
+            if (cam != null && cam.GetComponent<SSS>() == null)
+            {
+                SSS camSSS = cam.gameObject.AddComponent<SSS>();
+                camSSS.enabled = true;
+                camSSS.Enabled = true;
+                SSSManager.RegisterAdditionalInstance(camSSS);
+                Graphics.Instance.Log.LogInfo($"Adding SSS Component to Camera: {cam.name} GO: {cam.gameObject.name}");
+            }
+        }
+        
 
         private void OnDisable()
         {
