d19f3f987611d088ea0bf3fd36af6bee7c199337
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-11-16 15:19:38 -0700

    Version Change

--- Graphics/Shared/Graphics.cs
+++ Graphics/Shared/Graphics.cs
@@ -25,7 +25,7 @@ namespace Graphics
     {
         public const string GUID = "ore.graphics";
         public const string PluginName = "Graphics";
-        public const string Version = "0.4.7";
+        public const string Version = "0.4.8";
 
         public static ConfigEntry<KeyCode> ConfigShortcut { get; private set; }
         public static ConfigEntry<string> ConfigCubeMapPath { get; private set; }
