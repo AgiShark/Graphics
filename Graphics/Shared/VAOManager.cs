c0c52d06edb1756ff2185529dfb637de479e77b5
Hanmen <45272500+Hanmen-lab@users.noreply.github.com>
2021-12-16 04:39:10 +0300

    Add files via upload

---
+++ Graphics/Shared/VAOManager.cs
@@ -0,0 +1,98 @@
+﻿using Graphics.Settings;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Graphics.VAO
+{
+    public class VAOManager
+    {
+        public static Settings.VAOSettings settings;
+
+        internal static VAOEffect VAOInstance;
+        private static List<VAOEffect> otherVAOInstances = new List<VAOEffect>();
+
+        // Initialize Components
+        internal void Initialize()
+        {
+            VAOInstance = Graphics.Instance.CameraSettings.MainCamera.GetOrAddComponent<VAOEffect>();
+            if (settings == null)
+                settings = new VAOSettings();
+
+            settings.Load(VAOInstance);
+            CopySettingsToOtherInstances();
+        }
+
+        // When user enabled the option
+        internal void Start()
+        {
+        }
+
+        public static void RegisterAdditionalInstance(VAOEffect otherInstance)
+        {
+            if (!otherVAOInstances.Contains(otherInstance))
+            {
+                otherVAOInstances.Add(otherInstance);
+                VAOManager.CopySettingsToOtherInstances();
+            }
+        }
+        public static void UpdateSettings()
+        {
+            if (settings == null)
+                settings = new VAOSettings();
+            if (VAOInstance != null)
+                settings.Load(VAOInstance);
+
+            CopySettingsToOtherInstances();
+        }
+
+        internal static void CopySettingsToOtherInstances()
+        {
+            foreach (VAOEffect otherInstance in otherVAOInstances)
+            {
+                settings.Load(otherInstance);
+            }
+        }
+
+        // When user disabled the option
+        internal void Destroy()
+        {
+            DestroyVAOInstance(VAOInstance);
+            for (int i = otherVAOInstances.Count - 1; i >= 0; i--)
+            {
+                VAOEffect otherInstance = otherVAOInstances[i];
+                if (otherInstance == null)
+                {
+                }
+                else
+                {
+                    otherInstance.enabled = VAOInstance.enabled;
+                    DestroyVAOInstance(otherInstance);
+
+                }
+            }
+        }
+
+        public static void DestroyVAOInstance(VAOEffect VAOInstance)
+        {
+            otherVAOInstances.Remove(VAOInstance);
+        }
+
+        IEnumerator WaitForCamera()
+        {
+            Camera camera = Graphics.Instance.CameraSettings.MainCamera;
+            yield return new WaitUntil(() => camera != null);
+            CheckInstance();
+        }
+        public void CheckInstance()
+        {
+            if (VAOInstance == null)
+            {
+                Camera camera = Graphics.Instance.CameraSettings.MainCamera;
+                VAOInstance = camera.GetOrAddComponent<VAOEffect>();
+            }
+        }
+    }
+}
